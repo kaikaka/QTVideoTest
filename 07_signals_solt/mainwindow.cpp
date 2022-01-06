f54f4c819cf86d1186713d73bb27a66105658a3f
yoon <yinxiangkai520@163.com>
2022-01-06 22:13:46 +0800

    [UI信号与槽]

---
+++ 07_signals_solt/mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug() << "on_pushButton_clicked";
+}
+
+
+void MainWindow::on_login_clicked()
+{
+    qDebug() << "on_login_clicked";
+}
+
