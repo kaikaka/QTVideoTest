a3f73c1c710d9c94a16262bad1b6251448d3bb5f
kai.yin <kai.yin@wehotelglobal.com>
2022-03-03 16:36:51 +0800

    [sdl 播放 wav文件]

---
+++ 12_sdl_play_wav/mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "playthread.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void showSDLVersion();
+private slots:
+    void on_audioButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    playThread *_playThread = nullptr;
+};
+#endif // MAINWINDOW_H
